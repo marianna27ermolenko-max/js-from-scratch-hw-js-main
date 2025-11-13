c5911d886099cba2b17b1b93ab3a71db4f96ef98
Marianna Drozd <marianna27ermolenko@gmail.com>
2025-11-13 13:20:47 +0300

    lesson-03-02-done

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,21 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+
+// ПЕРВЫЙ ВАРИАНТ
+// function isEven(number) {
+//     if (number%2 === 0) {
+//         console.log(true);
+        
+//     } else {
+//          console.log(false)
+//     }
+// }
+
+
+function isEven(number) {
+
+     let answer = number%2 === 0 ? true : false;   
+     return answer
+    
+}
